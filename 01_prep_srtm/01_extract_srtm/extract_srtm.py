9c516421901a4114ff56487bb94ed10c4eedc37b
Pete Bunting <petebunting@mac.com>
2022-05-27 08:52:35 +0100

    Create output directory if it does not exist.

--- 01_prep_srtm/01_extract_srtm/extract_srtm.py
+++ 01_prep_srtm/01_extract_srtm/extract_srtm.py
@@ -8,6 +8,8 @@ for in_file in input_files:
     tile = os.path.basename(in_file).split('.')[0]
     print(tile)
     out_dir = os.path.join(out_base_dir, tile)
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
     rsgislib.tools.filetools.unzip_file(in_file, out_dir)
 
 
