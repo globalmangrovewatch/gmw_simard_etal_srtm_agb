cb938471478f037f5a428010c9b4f243fcdb3639
Pete Bunting <petebunting@mac.com>
2022-05-27 08:28:36 +0100

    Add script to prepare SRTM dataset for analysis.

---
+++ 01_prep_srtm/01_extract_srtm/extract_srtm.py
@@ -0,0 +1,13 @@
+import glob
+import os
+import rsgislib.tools.filetools
+
+out_base_dir = '/scratch/a.pfb/gmw_simard_etal_srtm_agb/data/srtm/hgt'
+input_files = glob.glob('/scratch/a.pfb/gmw_simard_etal_srtm_agb/data/srtm/raw/*.zip')
+for in_file in input_files:
+    tile = os.path.basename(in_file).split('.')[0]
+    print(tile)
+    out_dir = os.path.join(out_base_dir, tile)
+    rsgislib.tools.filetools.unzip_file(in_file, out_dir)
+
+
