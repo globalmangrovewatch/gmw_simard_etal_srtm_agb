8abc504a89ecb9c3830086087451e302b634ace5
Pete Bunting <petebunting@mac.com>
2022-05-27 14:57:04 +0100

    Update adding country and allometric info to countries vector layer.

--- 03_define_country_extents/01_define_country_ids/create_country_ids.py
+++ 03_define_country_extents/01_define_country_ids/create_country_ids.py
@@ -1,5 +1,7 @@
 import rsgislib.vectorattrs
 import rsgislib.tools.utils
+import numpy
+import osgeo.ogr as ogr
 
 out_vec_file="/scratch/a.pfb/gmw_simard_etal_srtm_agb/data/countries/GADM_EEZ_WCMC_UnqID.gpkg"
 out_vec_lyr="National"
@@ -24,4 +26,40 @@ for ref_val, unq_val in zip(ref_vals, unq_vals):
     lut['ref'][ref_val] = unq_val
     lut['val'][unq_val] = ref_val
 
-rsgislib.tools.utils.write_dict_to_json(lut, )
+rsgislib.tools.utils.write_dict_to_json(lut, "country_ids_lut.json")
+
+
+cntry_names = numpy.empty(len(ref_vals), dtype=numpy.dtype('U255'))
+agb_allom_rgns = numpy.empty(len(ref_vals), dtype=numpy.dtype('U255'))
+agb_allom_rgns_idx = numpy.zeros(len(ref_vals), dtype=numpy.dtype('int'))
+agb_allom_rgns[...] = 'Global Hmax power'
+
+country_names_lut = rsgislib.tools.utils.read_json_to_dict("gadm_lut.json")
+agb_allom_lut = rsgislib.tools.utils.read_json_to_dict("countries_abg_lut.json")
+
+agb_allom_id_lut = dict()
+agb_allom_id_lut["id"] = dict()
+agb_allom_id_lut["allom"] = dict()
+id = 1
+for cntry in agb_allom_lut:
+    if agb_allom_lut[cntry] not in agb_allom_id_lut["allom"]:
+        agb_allom_id_lut["allom"][agb_allom_lut[cntry]] = id
+        agb_allom_id_lut["id"][id] = agb_allom_lut[cntry]
+        id = id + 1
+
+for i, cntry_id in enumerate(ref_vals):
+    if cntry_id in country_names_lut["gid"]:
+        country_name = country_names_lut["gid"][cntry_id]
+        cntry_names[i] = country_name
+        if country_name in agb_allom_lut:
+            agb_allom = agb_allom_lut[country_name]
+            agb_allom_rgns[i] = agb_allom
+            agb_allom_rgns_idx[i] = agb_allom_id_lut["allom"][country_name]
+
+
+rsgislib.vectorattrs.write_vec_column(out_vec_file, out_vec_lyr, "country_names", ogr.OFTString, cntry_names.tolist())
+rsgislib.vectorattrs.write_vec_column(out_vec_file, out_vec_lyr, "agb_allom", ogr.OFTString, agb_allom_rgns.tolist())
+rsgislib.vectorattrs.write_vec_column(out_vec_file, out_vec_lyr, "agb_allom", ogr.OFTInteger, agb_allom_rgns_idx.tolist())
+
+rsgislib.tools.utils.write_dict_to_json("allom_id_lut.json", agb_allom_id_lut)
+
