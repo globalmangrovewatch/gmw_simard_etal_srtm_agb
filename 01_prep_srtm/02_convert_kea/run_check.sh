c2826834fe60afc9327382aaff2511dd5c7dd852
Pete Bunting <petebunting@mac.com>
2022-05-27 13:56:56 +0100

    Update to remove error files.

--- 01_prep_srtm/02_convert_kea/run_check.sh
+++ 01_prep_srtm/02_convert_kea/run_check.sh
@@ -3,5 +3,5 @@
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb \
 /scratch/a.pfb/sw_imgs/au-eoed-dev.sif \
 rsgischkgdalfile.py -i "/scratch/a.pfb/gmw_simard_etal_srtm_agb/data/srtm/kea/*.kea" \
---nbands 1 --epsg 4326 --chkproj --readimg --chksum
+--nbands 1 --epsg 4326 --chkproj --readimg --chksum --rmerr --printerrs
 
