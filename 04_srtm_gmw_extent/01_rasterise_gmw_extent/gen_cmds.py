d3ca3e653a1532b65d030806f3a2f0c888aebbc8
Pete Bunting <petebunting@mac.com>
2022-06-01 08:01:57 +0200

    Update to output file name.

--- 04_srtm_gmw_extent/01_rasterise_gmw_extent/gen_cmds.py
+++ 04_srtm_gmw_extent/01_rasterise_gmw_extent/gen_cmds.py
@@ -17,7 +17,7 @@ class GenTaskCmds(PBPTGenQProcessToolCmds):
         srtm_tiles = glob.glob(kwargs['srtm_tiles'])
         for srtm_tile in srtm_tiles:
             tile_base_name = rsgislib.tools.filetools.get_file_basename(srtm_tile)
-            out_img = os.path.join(kwargs['out_path'], "{}.kea".format(tile_base_name))
+            out_img = os.path.join(kwargs['out_path'], "{}_gmw_union.kea".format(tile_base_name))
             if not os.path.exists(out_img):
                 c_dict = dict()
                 c_dict['srtm_tile'] = srtm_tile
