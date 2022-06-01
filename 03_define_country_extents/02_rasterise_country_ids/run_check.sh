a8c4af22bd52abfb230606ad1bef29b3493cc5ea
Pete Bunting <petebunting@mac.com>
2022-06-01 10:50:11 +0100

    Update to remove error files

--- 03_define_country_extents/02_rasterise_country_ids/run_check.sh
+++ 03_define_country_extents/02_rasterise_country_ids/run_check.sh
@@ -3,10 +3,10 @@
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb \
 /scratch/a.pfb/sw_imgs/au-eoed-dev.sif \
 rsgischkgdalfile.py -i "/scratch/a.pfb/gmw_simard_etal_srtm_agb/data/countries/srtm_rasters/*.kea" \
---nbands 1 --epsg 4326 --chkproj --readimg --chksum
+--nbands 1 --epsg 4326 --chkproj --readimg --chksum --rmerr
 
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb \
 /scratch/a.pfb/sw_imgs/au-eoed-dev.sif \
 rsgischkgdalfile.py -i "/scratch/a.pfb/gmw_simard_etal_srtm_agb/data/countries/agb_allom_rasters/*.kea" \
---nbands 1 --epsg 4326 --chkproj --readimg --chksum
+--nbands 1 --epsg 4326 --chkproj --readimg --chksum --rmerr
 
--- 04_srtm_gmw_extent/02_gmw_mask_srtm/run_check.sh
+++ 04_srtm_gmw_extent/02_gmw_mask_srtm/run_check.sh
@@ -3,6 +3,6 @@
 singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb \
 /scratch/a.pfb/sw_imgs/au-eoed-dev.sif \
 rsgischkgdalfile.py -i "/scratch/a.pfb/gmw_simard_etal_srtm_agb/data/srtm/gmw_srtm_kea/*.kea" \
---nbands 1 --epsg 4326 --chkproj --readimg --chksum
+--nbands 1 --epsg 4326 --chkproj --readimg --chksum --rmerr
 
 
