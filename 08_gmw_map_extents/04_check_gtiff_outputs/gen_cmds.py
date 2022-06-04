9f9f078b79f562c835eb472d282a53373d3dff46
Pete Bunting <petebunting@mac.com>
2022-06-04 22:56:24 +0100

    Add scripts to check tif and kea images are identical.

---
+++ 08_gmw_map_extents/04_check_gtiff_outputs/gen_cmds.py
@@ -0,0 +1,71 @@
+from pbprocesstools.pbpt_q_process import PBPTGenQProcessToolCmds
+
+import logging
+import os
+import glob
+
+import rsgislib.tools.filetools
+import rsgislib.vectorutils
+
+logger = logging.getLogger(__name__)
+
+class GenTaskCmds(PBPTGenQProcessToolCmds):
+
+    def gen_command_info(self, **kwargs):
+        if not os.path.exists(kwargs['out_path']):
+            os.mkdir(kwargs['out_path'])
+
+        img_tiles = glob.glob(kwargs['img_tiles'])
+        for img_tile in img_tiles:
+            tile_base_name = rsgislib.tools.filetools.get_file_basename(img_tile)
+            tif_img = os.path.join(kwargs['tif_path'], "{}.tif".format(tile_base_name))
+            out_file = os.path.join(kwargs['out_path'], "{}_chk.json".format(tile_base_name))
+            if not os.path.exists(out_file):
+                c_dict = dict()
+                c_dict['img_tile'] = img_tile
+                c_dict['tif_img'] = tif_img
+                c_dict['out_file'] = out_file
+                self.params.append(c_dict)
+
+    def run_gen_commands(self):
+        vec_file = "/scratch/a.pfb/gmw_simard_etal_srtm_agb/data/gmw/gmw_v3_fnl_mjr_v314.gpkg"
+
+        vec_lyrs = rsgislib.vectorutils.get_vec_lyrs_lst(vec_file)
+        for vec_lyr in vec_lyrs:
+            self.gen_command_info(img_tiles='/scratch/a.pfb/gmw_simard_etal_srtm_agb/data/gmw_srtm_mangrove_agb/agb_{}/*.kea'.format(vec_lyr),
+                                  tif_path='/scratch/a.pfb/gmw_simard_etal_srtm_agb/data/gmw_srtm_mangrove_agb/agb_{}_tif'.format(vec_lyr),
+                                  out_path='/scratch/a.pfb/gmw_simard_etal_srtm_agb/data/gmw_srtm_mangrove_agb/agb_{}_tif_chkd'.format(vec_lyr))
+
+        for vec_lyr in vec_lyrs:
+            self.gen_command_info(val_tiles='/scratch/a.pfb/gmw_simard_etal_srtm_agb/data/gmw_srtm_mangrove_hgt/hmax_{}/*.kea'.format(vec_lyr),
+                                  tif_path='/scratch/a.pfb/gmw_simard_etal_srtm_agb/data/gmw_srtm_mangrove_hgt/hmax_{}_tif'.format(vec_lyr),
+                                  out_path='/scratch/a.pfb/gmw_simard_etal_srtm_agb/data/gmw_srtm_mangrove_hgt/hmax_{}_tif_chkd'.format(vec_lyr))
+
+        for vec_lyr in vec_lyrs:
+            self.gen_command_info(val_tiles='/scratch/a.pfb/gmw_simard_etal_srtm_agb/data/gmw_srtm_mangrove_hgt/hba_{}/*.kea'.format(vec_lyr),
+                                  tif_path='/scratch/a.pfb/gmw_simard_etal_srtm_agb/data/gmw_srtm_mangrove_hgt/hba_{}_tif'.format(vec_lyr),
+                                  out_path='/scratch/a.pfb/gmw_simard_etal_srtm_agb/data/gmw_srtm_mangrove_hgt/hba_{}_tif_chkd'.format(vec_lyr))
+
+        for vec_lyr in vec_lyrs:
+            self.gen_command_info(val_tiles='/scratch/a.pfb/gmw_simard_etal_srtm_agb/data/gmw_srtm_mangrove_hgt/hchm_{}/*.kea'.format(vec_lyr),
+                                  tif_path='/scratch/a.pfb/gmw_simard_etal_srtm_agb/data/gmw_srtm_mangrove_hgt/hchm_{}_tif'.format(vec_lyr),
+                                  out_path='/scratch/a.pfb/gmw_simard_etal_srtm_agb/data/gmw_srtm_mangrove_hgt/hchm_{}_tif_chkd'.format(vec_lyr))
+
+        self.pop_params_db()
+        self.create_slurm_sub_sh("convert_to_gtiff", 8224, '/scratch/a.pfb/gmw_simard_etal_srtm_agb/logs',
+                                 run_script='run_exe_analysis.sh', job_dir="job_scripts",
+                                 db_info_file="db_info_run_file.txt", account_name='scw1376',
+                                 n_cores_per_job=10, n_jobs=10, job_time_limit='2-23:59',
+                                 module_load='module load parallel singularity\n\nexport http_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\nexport https_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\n')
+
+if __name__ == "__main__":
+    py_script = os.path.abspath("perform_analysis.py")
+    script_cmd = "singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python {}".format(py_script)
+
+    process_tools_mod = 'perform_analysis'
+    process_tools_cls = 'PerformAnalysis'
+
+    create_tools = GenTaskCmds(cmd=script_cmd, db_conn_file="/home/a.pfb/gmw_gap_fill_db/pbpt_db_conn.txt",
+                                         lock_file_path="./gmw_agb_lock_file.txt",
+                                         process_tools_mod=process_tools_mod, process_tools_cls=process_tools_cls)
+    create_tools.parse_cmds()
---
+++ 08_gmw_map_extents/04_check_gtiff_outputs/perform_analysis.py
@@ -0,0 +1,39 @@
+from pbprocesstools.pbpt_q_process import PBPTQProcessTool
+import logging
+import os
+import rsgislib
+import rsgislib.imagecalc
+import rsgislib.tools.utils
+
+logger = logging.getLogger(__name__)
+
+
+class PerformAnalysis(PBPTQProcessTool):
+    def __init__(self):
+        super().__init__(cmd_name="perform_analysis.py", descript=None)
+
+    def do_processing(self, **kwargs):
+        if os.path.exists(self.params['tif_img']):
+            match, prop_diff = rsgislib.imagecalc.are_imgs_equal(self.params['img_tile'], self.params['tif_img'], prop_eql=1.0, flt_dif=0.0001)
+            if not match:
+                os.remove(self.params['tif_img'])
+            else:
+                match_info = dict()
+                match_info['match'] = match
+                match_info['prop_diff'] = float(prop_diff)
+                rsgislib.tools.utils.write_dict_to_json(match_info, self.params['out_file'])
+
+    def required_fields(self, **kwargs):
+        return ["img_tile", "tif_img", "out_file"]
+
+    def outputs_present(self, **kwargs):
+        files_dict = dict()
+        files_dict[self.params["out_file"]] = 'file'
+        return self.check_files(files_dict)
+
+    def remove_outputs(self, **kwargs):
+        if os.path.exists(self.params["out_file"]):
+            os.remove(self.params["out_file"])
+
+if __name__ == "__main__":
+    PerformAnalysis().std_run()
