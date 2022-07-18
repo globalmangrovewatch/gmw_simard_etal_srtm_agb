6ba883a7afcb30695c39a485858ba082d86d9604
Pete Bunting <petebunting@mac.com>
2022-07-18 20:00:45 +0100

    Update with scripts for height histograms.

---
+++ 10_calc_hgt_stats/03_calc_tile_hist/gen_cmds.py
@@ -0,0 +1,55 @@
+from pbprocesstools.pbpt_q_process import PBPTGenQProcessToolCmds
+
+import logging
+import os
+import glob
+
+import rsgislib.tools.filetools
+import rsgislib.vectorutils
+
+logger = logging.getLogger(__name__)
+
+class GenTaskCmds(PBPTGenQProcessToolCmds):
+
+    def gen_command_info(self, **kwargs):
+        if not os.path.exists(kwargs['out_path']):
+            os.mkdir(kwargs['out_path'])
+
+        agb_tiles = glob.glob(kwargs['agb_tiles'])
+        for agb_tile in agb_tiles:
+            tile_agb_name = rsgislib.tools.filetools.get_file_basename(agb_tile)
+            tile_base_name = tile_agb_name.replace("_agb_gmw_v314_mng_mjr_2020", "")
+            cntry_img = os.path.join(kwargs['cntry_uid_dir'], "{}_cnty.kea".format(tile_base_name))
+
+            out_file = os.path.join(kwargs['out_path'], "{}_country_hists.json".format(tile_agb_name))
+            if not os.path.exists(out_file):
+                c_dict = dict()
+                c_dict['agb_tile'] = agb_tile
+                c_dict['country_ids_lut_file'] = kwargs['country_ids_lut_file']
+                c_dict['cntry_img'] = cntry_img
+                c_dict['out_file'] = out_file
+                self.params.append(c_dict)
+
+    def run_gen_commands(self):
+        self.gen_command_info(agb_tiles='/home/pete/Documents/gmw_v3_agb_hgt/gmw_srtm_mangrove_agb/agb_mng_mjr_2020_tif/*.tif',
+                              cntry_uid_dir='/home/pete/Documents/gmw_v3_agb_hgt/countries/srtm_rasters',
+                              country_ids_lut_file='../../03_define_country_extents/01_define_country_ids/country_ids_lut.json',
+                              gmw_extent_dirs='/home/pete/Documents/gmw_v3_agb_hgt/gmw/mng_mjr_2020',
+                              out_path='/home/pete/Documents/gmw_v3_agb_hgt/stats/hist_tile_stats')
+
+        self.pop_params_db()
+
+        self.create_shell_exe(run_script="run_exe_analysis.sh", cmds_sh_file="cmds_lst.sh", n_cores=20, db_info_file="gmw_agb_db_conn_info.json")
+
+
+if __name__ == "__main__":
+    py_script = os.path.abspath("perform_analysis.py")
+    script_cmd = "python {}".format(py_script)
+
+    process_tools_mod = 'perform_analysis'
+    process_tools_cls = 'PerformAnalysis'
+
+    create_tools = GenTaskCmds(cmd=script_cmd, db_conn_file="/home/pete/.pbpt_db_conn.txt",
+                                         lock_file_path="./gmw_agb_lock_file.txt",
+                                         process_tools_mod=process_tools_mod, process_tools_cls=process_tools_cls)
+    create_tools.parse_cmds()
---
+++ 10_calc_hgt_stats/03_calc_tile_hist/perform_analysis.py
@@ -0,0 +1,80 @@
+from pbprocesstools.pbpt_q_process import PBPTQProcessTool
+import logging
+import os
+import numpy
+import osgeo.gdal as gdal
+import rsgislib.tools.utils
+
+logger = logging.getLogger(__name__)
+
+
+def calc_unq_val_pxl_areas(vals_img, uid_img, unq_val_area_lut):
+    img_vals_ds = gdal.Open(vals_img)
+    if img_vals_ds is None:
+        raise Exception("Could not open the values input image: '{}'".format(uid_img))
+    img_vals_band = img_vals_ds.GetRasterBand(1)
+    if img_vals_band is None:
+        raise Exception("Failed to read the values image band: '{}'".format(uid_img))
+    vals_arr = img_vals_band.ReadAsArray()
+    img_vals_ds = None
+
+    img_uid_ds = gdal.Open(uid_img)
+    if img_uid_ds is None:
+        raise Exception("Could not open the UID input image: '{}'".format(uid_img))
+    img_uid_band = img_uid_ds.GetRasterBand(1)
+    if img_uid_band is None:
+        raise Exception("Failed to read the UID image band: '{}'".format(uid_img))
+    uid_arr = img_uid_band.ReadAsArray()
+    img_uid_ds = None
+
+    unq_pix_vals = numpy.unique(uid_arr)
+
+    for unq_val in unq_pix_vals:
+        if unq_val != 0:
+            msk = numpy.zeros_like(uid_arr, dtype=bool)
+            msk[(uid_arr == unq_val) & (uid_arr > 0) & (vals_arr > 0)] = True
+
+            if numpy.sum(msk) > 0:
+                vals_unq_val_arr = vals_arr[msk].flatten()
+                print(vals_unq_val_arr.shape)
+                vals_hist, bin_edges = numpy.histogram(vals_unq_val_arr, bins=71, range=(0, 71))
+                print(bin_edges)
+                print(vals_hist.shape)
+                print(unq_val_area_lut[unq_val].shape)
+                unq_val_area_lut[unq_val] = unq_val_area_lut[unq_val] + vals_hist
+
+
+class PerformAnalysis(PBPTQProcessTool):
+    def __init__(self):
+        super().__init__(cmd_name="perform_analysis.py", descript=None)
+
+    def do_processing(self, **kwargs):
+        unq_vals = rsgislib.tools.utils.read_json_to_dict(self.params['country_ids_lut_file'])["val"].keys()
+
+        tile_stats_lut = dict()
+
+        lut_vals = dict()
+        for val in unq_vals:
+            val = int(val)
+            lut_vals[val] = numpy.zeros((71), dtype=numpy.uint32)
+
+        calc_unq_val_pxl_areas(self.params["agb_tile"], self.params["cntry_img"], lut_vals)
+
+        rsgislib.tools.utils.write_dict_to_json(lut_vals, self.params["out_file"])
+
+
+    def required_fields(self, **kwargs):
+        return ["agb_tile", "country_ids_lut_file", "cntry_img", "out_file"]
+
+    def outputs_present(self, **kwargs):
+        files_dict = dict()
+        files_dict[self.params["out_file"]] = {"type": "file"}
+        return self.check_files(files_dict)
+
+    def remove_outputs(self, **kwargs):
+        if os.path.exists(self.params["out_file"]):
+            os.remove(self.params["out_file"])
+
+
+if __name__ == "__main__":
+    PerformAnalysis().std_run()
