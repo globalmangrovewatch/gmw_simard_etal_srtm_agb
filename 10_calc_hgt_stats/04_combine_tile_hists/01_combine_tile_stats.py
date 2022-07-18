31fd1e7ca00e5c29e7e4cdeee573232c8110849f
Pete Bunting <petebunting@mac.com>
2022-07-18 20:55:39 +0100

    Scripts to merge hchm histograms.

---
+++ 10_calc_hgt_stats/04_combine_tile_hists/01_combine_tile_stats.py
@@ -0,0 +1,22 @@
+import glob
+import rsgislib.tools.utils
+import numpy
+
+country_ids_lut_file='../../03_define_country_extents/01_define_country_ids/country_ids_lut.json'
+unq_cntry_vals = rsgislib.tools.utils.read_json_to_dict(country_ids_lut_file)["val"].keys()
+
+tile_stats_lut = dict()
+
+for val in unq_cntry_vals:
+    tile_stats_lut[val] = numpy.zeros((71), dtype=numpy.uint32)
+
+stats_tiles = glob.glob('/home/pete/Documents/gmw_v3_agb_hgt/stats/hist_hchm_tile_stats/*.json')
+
+for stats_tile_file in stats_tiles:
+    stats_tile_lut = rsgislib.tools.utils.read_json_to_dict(stats_tile_file)
+
+    for val in unq_cntry_vals:
+        tile_stats_lut[val] += numpy.array(stats_tile_lut[val], dtype=numpy.uint32)
+
+rsgislib.tools.utils.write_dict_to_json(tile_stats_lut, '/home/pete/Documents/gmw_v3_agb_hgt/stats/country_hchm_hists.json')
+
