44e72d3ea1a299300d26d4fc2646be3e32691a96
Pete Bunting <petebunting@mac.com>
2022-07-18 20:08:46 +0100

    Update to use ranges for AGB.

--- 09_calc_agb_stats/04_calc_tile_hist/perform_analysis.py
+++ 09_calc_agb_stats/04_calc_tile_hist/perform_analysis.py
@@ -37,7 +37,7 @@ def calc_unq_val_pxl_areas(vals_img, uid_img, unq_val_area_lut):
             if numpy.sum(msk) > 0:
                 vals_unq_val_arr = vals_arr[msk].flatten()
                 print(vals_unq_val_arr.shape)
-                vals_hist, bin_edges = numpy.histogram(vals_unq_val_arr, bins=71, range=(0, 71))
+                vals_hist, bin_edges = numpy.histogram(vals_unq_val_arr, bins=81, range=(0, 2025))
                 print(bin_edges)
                 print(vals_hist.shape)
                 print(unq_val_area_lut[unq_val].shape)
@@ -56,7 +56,7 @@ class PerformAnalysis(PBPTQProcessTool):
         lut_vals = dict()
         for val in unq_vals:
             val = int(val)
-            lut_vals[val] = numpy.zeros((100), dtype=numpy.uint32)
+            lut_vals[val] = numpy.zeros((81), dtype=numpy.uint32)
 
         calc_unq_val_pxl_areas(self.params["agb_tile"], self.params["cntry_img"], lut_vals)
 
