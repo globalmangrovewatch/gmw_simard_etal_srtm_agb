d23d43fa1ad8c3ca055ba4f6d215dd73b7a60245
Pete Bunting <petebunting@mac.com>
2026-04-12 21:48:18 +0200

    Updated.

--- 05_apply_95th_percentile_threshold/01_calc_tile_histograms/gen_cmds.py
+++ 05_apply_95th_percentile_threshold/01_calc_tile_histograms/gen_cmds.py
@@ -8,7 +8,7 @@ import rsgislib.tools.filetools
 
 logger = logging.getLogger(__name__)
 
-class GenTaskCmds(PBPTGenQProcessToolCmds):
+class GenCmds(PBPTGenQProcessToolCmds):
 
     def gen_command_info(self, **kwargs):
         if not os.path.exists(kwargs['out_path']):
@@ -17,7 +17,7 @@ class GenTaskCmds(PBPTGenQProcessToolCmds):
         srtm_tiles = glob.glob(kwargs['srtm_tiles'])
         for srtm_tile in srtm_tiles:
             tile_base_name = rsgislib.tools.filetools.get_file_basename(srtm_tile).replace("_srtm_gmw", "")
-            cnty_img = os.path.join(kwargs['cnty_path'], "{}_cnty.kea".format(tile_base_name))
+            cnty_img = os.path.join(kwargs['cnty_path'], "{}_cntry.tif".format(tile_base_name))
             out_file = os.path.join(kwargs['out_path'], "{}_srtm_cnty_hist.pkl".format(tile_base_name))
             if not os.path.exists(out_file):
                 c_dict = dict()
@@ -28,25 +28,31 @@ class GenTaskCmds(PBPTGenQProcessToolCmds):
 
     def run_gen_commands(self):
 
-        self.gen_command_info(srtm_tiles='/scratch/a.pfb/gmw_simard_etal_srtm_agb/data/srtm/gmw_srtm_kea/*.kea',
-                              cnty_path="/scratch/a.pfb/gmw_simard_etal_srtm_agb/data/countries/srtm_rasters",
-                              out_path='/scratch/a.pfb/gmw_simard_etal_srtm_agb/data/srtm/cnty_tile_hists')
+        self.gen_command_info(srtm_tiles='/bigdata/petebunting/GlobalMangroveWatch/gmw_blue_carbon_v4_ext/simard_srtm_agb/data/srtm/gmw_v4109_2000_srtm_tiles/*.tif',
+                              cnty_path="/bigdata/petebunting/GlobalMangroveWatch/gmw_blue_carbon_v4_ext/simard_srtm_agb/data/countries/srtm_rasters",
+                              out_path='/bigdata/petebunting/GlobalMangroveWatch/gmw_blue_carbon_v4_ext/simard_srtm_agb/data/srtm/cnty_tile_hists')
 
         self.pop_params_db()
-        self.create_slurm_sub_sh("apply_gmw_to_srtm", 8224, '/scratch/a.pfb/gmw_simard_etal_srtm_agb/logs',
-                                 run_script='run_exe_analysis.sh', job_dir="job_scripts",
-                                 db_info_file="db_info_run_file.txt", account_name='scw1376',
-                                 n_cores_per_job=10, n_jobs=10, job_time_limit='2-23:59',
-                                 module_load='module load parallel singularity\n\nexport http_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\nexport https_proxy="http://a.pfb:proxy101019@10.212.63.246:3128"\n')
 
-if __name__ == "__main__":
-    py_script = os.path.abspath("perform_analysis.py")
-    script_cmd = "singularity exec --bind /scratch/a.pfb:/scratch/a.pfb --bind /home/a.pfb:/home/a.pfb /scratch/a.pfb/sw_imgs/au-eoed-dev.sif python {}".format(py_script)
+        self.create_shell_exe(
+                run_script="run_exe_analysis.sh",  # The file to call to run analysis
+                cmds_sh_file="pbpt_cmds_lst.sh",  # The list of commands to be run.
+                n_cores=40,  # The number of cores to use for analysis.
+                db_info_file="pbpt_lcl_db_info.json",
+        )
 
-    process_tools_mod = 'perform_analysis'
-    process_tools_cls = 'PerformAnalysis'
+    if __name__ == "__main__":
+        py_script = os.path.abspath("perform_analysis.py")
+        script_cmd = f"python {py_script}"
 
-    create_tools = GenTaskCmds(cmd=script_cmd, db_conn_file="/home/a.pfb/gmw_gap_fill_db/pbpt_db_conn.txt",
-                                         lock_file_path="./gmw_agb_lock_file.txt",
-                                         process_tools_mod=process_tools_mod, process_tools_cls=process_tools_cls)
-    create_tools.parse_cmds()
+        process_tools_mod = "perform_analysis"
+        process_tools_cls = "PerformAnalysis"
+
+        create_tools = GenCmds(
+                cmd=script_cmd,
+                db_conn_file="/home/pete/.pbpt_db_conn.txt",
+                lock_file_path="./pbpt_lock_file.txt",
+                process_tools_mod=process_tools_mod,
+                process_tools_cls=process_tools_cls,
+        )
+        create_tools.parse_cmds()
